746ffc74faecfa8bc10c6fc40e7d651422caec80
vikas9872 <shanabhogvikas648.com>
2025-05-23 15:52:40 +0530

    signup

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -11,30 +11,30 @@ const Register = () => {
     try {
       const email = emailRef.current.value;
       const pass = passRef.current.value;
-      const createuserwithemailandpassword = await createUserWithEmailAndPassword(auth, email, pass);
-      const user = createuserwithemailandpassword.user;
-      alert("User created")
+      const loginuserwithemailandpassword = await signInWithEmailAndPassword(auth, email, pass);
+      const user = loginuserwithemailandpassword.user;
+      alert("User logged in");
     } catch (error) {
       console.log(error);
     }
   }
-  const handleLogIn=async (e)=>{
+  const handleSignUp = async (e) => {
     e.preventDefault();
-    try{
-      const email=emailRef.current.value;
-      const pass=passRef.current.value;
-      const loginuserwithemailandpassword=await signInWithEmailAndPassword(auth,email,pass);
-      const loguser=loginuserwithemailandpassword.loguser;
-      alert("User logged in")
-    }catch(error){
-      console.log(error)
+    try {
+      const email = emailRef.current.value;
+      const pass = passRef.current.value;
+      const createuserwithemailandpassword = await createUserWithEmailAndPassword(auth, email, pass);
+      const user = createuserwithemailandpassword.user;
+      alert("User created");
+    } catch (error) {
+      console.log(error);
     }
   }
   return (
     <div className="flex justify-center items-center min-h-screen w-screen bg-gray-50">
       <div className="border-2 rounded-lg w-[90%] md:w-[50%] lg:w-[40%] p-6 bg-white shadow-lg">
         <h2 className="font-extrabold text-4xl text-center mb-6">
-          {isSignInMode ? 'Sign In' : 'Login'}
+          {isSignInMode ? 'Sign In' : 'Sign Up'}
         </h2>
         <form className="flex flex-col gap-6">
           <div>
@@ -57,36 +57,35 @@ const Register = () => {
               required
             />
           </div>
-          <div className="flex justify-center">
-            {isSignInMode
+          <div className="flex justify-center items-center">
+              {isSignInMode
               ? <button
                 className="text-white px-6 py-2 rounded-lg bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700"
                 onClick={handleSignIn}
               >
-                Sign in
+                Sign In
               </button>
               : <button
                 className="text-white px-6 py-2 rounded-lg bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700"
-                onClick={handleLogIn}
+                onClick={handleSignUp}
               >
-                Log in
+                Sign Up
               </button>
             }
-
           </div>
         </form>
         <div className="flex items-center justify-center mt-4">
           {isSignInMode
-            ? <p>Already have an account?</p>
-            : <p>Don't have an account?</p>
+            ? <p>Don't have an account?</p>
+            : <p>Already have an account?</p>
           }
           <button
             onClick={() => setIsSignInMode(!isSignInMode)}
             className="text-sm text-violet-500 hover:underline font-semibold cursor-pointer"
           >
             {isSignInMode
-              ? "Log in"
-              : "Sign up"}
+              ? "Sign Up"
+              : "Sign In"}
           </button>
         </div>
       </div>
